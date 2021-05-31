10f63fe9be9be190784164ec5df760cab0f4e20e
Jake <jigism@gmail.com>
2021-05-31 14:14:06 -0700

    'init'

---
+++ ActionClips.API/Controllers/Home.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActionClips.API.Controllers {
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase {
+
+        [HttpGet]
+        public string Index() {
+            return "Hello World";
+        }
+
+    }
+}
---
+++ ActionClips.API/Startup.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace ActionClips.API
+{
+    public class Startup
+    {
+        private const string CORS_ALL = "All";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+       
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddCors(options => options.AddPolicy(CORS_ALL, builder => builder
+                .AllowAnyHeader()
+                .AllowAnyOrigin()
+                .AllowAnyMethod()));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ActionClips.API", Version = "v1" });
+            });
+        }
+      
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ActionClips.API v1"));
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseCors(CORS_ALL);
+            app.UseRouting();            
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
